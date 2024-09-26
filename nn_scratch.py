9e2995315b229daca66df89d467a8d9c422264e9
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2024-09-26 14:40:49 +0530

    forward propagation

--- nn_scratch.py
+++ nn_scratch.py
@@ -2,4 +2,36 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_csv('train.csv')
-print(data)
+data = np.array(data)
+m,n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+
+def init_params():
+    W1 = np.random.randn(10, 784)
+    b1 = np.random.randn(10, 1)
+    W2 = np.random.randn(10, 10)
+    b2 = np.random.randn(10, 1)
+    return W1,W2,b1,b2
+
+def ReLU(Z):
+    return  np.maximum(0,Z)
+
+def Softmax(Z):
+    return np.exp(Z)/np.sum(np.exp(Z))
+
+def forward_prop(W1,W2,b1,b2,X,Y):
+    Z1 = np.dot(W1,X) + b1
+    A1 = ReLU(Z1)
+    Z2 = np.dot(W2,A1) + b2
+    A2 = Softmax(Z2)
+    return Z1,Z2,A1,A2
+
+
